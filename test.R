40201a4408e5413222a8690d1956ae7c46870dc0
luehkenecology <luehken65@gmail.com>
2018-11-20 09:39:37 +0100

    experiment with Imports/Depends in the DESCRIPTION-file

--- test.R
+++ test.R
@@ -2,9 +2,13 @@ library(devtools)
 devtools::load_all()
 devtools::document()
 
+print(.libPaths())
+library(RCurl)
 ?minDist()
 
 ?sumgfgfg
 
 ?sum_nums(1,2)
 hello()
+?dwd_down()
+elo <- dwd_down()
