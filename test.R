07962d5f3fc4e91abf095583bc8115bcc36e0219
luehkenecology <luehken65@gmail.com>
2018-11-20 16:25:19 +0100

    testing around

--- test.R
+++ test.R
@@ -2,13 +2,16 @@ library(devtools)
 devtools::load_all()
 devtools::document()
 
-print(.libPaths())
-library(RCurl)
-?minDist()
+UseMethod("plot")
+#minDist()
 
-?sumgfgfg
+#?sumgfgfg
 
-?sum_nums(1,2)
-hello()
-?dwd_down()
-elo <- dwd_down()
+#?sum_nums(1,2)
+#()
+btv()
+dwd_down()
+getwd()
+elo <- dwd_down(dwd_var = "air_temperature")
+eips(Temp = elo[[2]]$TT_TU, c("WNV", "DENV"))
+?alist
