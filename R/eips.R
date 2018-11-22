61a81428b1111e1706b267bcddf81740f935f109
luehkenecology <luehken65@gmail.com>
2018-11-22 07:10:11 +0100

    time scale of temperature input data

--- R/eips.R
+++ R/eips.R
@@ -1,16 +1,16 @@
 # Pathogen models
 fdf <- list()
-fdf[[1]] <- function(Temp){0.0003*Temp*(Temp-10.4057)} # BTV
-fdf[[2]] <- function(Temp){(-0.132+0.0092*Temp)} # WNV
-fdf[[3]] <- function(Temp){(0.019*(Temp-13.3))} # SBV
-fdf[[4]] <- function(Temp){(-0.1393+0.008*Temp)} # DENV
-fdf[[5]] <- function(Temp){(Temp-14/130)} # diro
-fdf[[6]] <- function(Temp){(0.000126*Temp+(Temp-14.244)*sqrt(34.4-Temp))} # malaria
+fdf[[1]] <- function(Temp, t_res){0.0003*Temp*(Temp-10.4057)/t_res} # BTV
+fdf[[2]] <- function(Temp, t_res){(-0.132+0.0092*Temp)/t_res} # WNV
+fdf[[3]] <- function(Temp, t_res){(0.019*(Temp-13.3))/t_res} # SBV
+fdf[[4]] <- function(Temp, t_res){(-0.1393+0.008*Temp)/t_res} # DENV
+fdf[[5]] <- function(Temp, t_res){(Temp-14/130)/t_res} # diro
+fdf[[6]] <- function(Temp, t_res){(0.000126*Temp+(Temp-14.244)*sqrt(34.4-Temp))/t_res} # malaria
 
 
-eips <- function(Temp, pathogen = "WNV"){
+eips <- function(Temp, pathogen = "WNV", t_res = 24){
   vec <-  which((c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria") %in% c(pathogen)) == T)
-  res <- lapply(fdf[c(vec)], function(x) x(Temp))
+  res <- lapply(fdf[c(vec)], function(x) x(Temp, t_res))
   do.call(cbind, res)
 }
 
