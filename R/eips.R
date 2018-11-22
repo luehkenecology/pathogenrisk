ceca498b3d8fcb0e15b4c12ac8f8be55f93cf587
luehkenecology <luehken65@gmail.com>
2018-11-22 15:37:53 +0100

    one eip function for dwd-data (eip_dwd) and one for general temperature-data-input (eip_simpel)

--- R/eips.R
+++ R/eips.R
@@ -7,12 +7,27 @@ fdf[[4]] <- function(Temp, t_res){(-0.1393+0.008*Temp)/t_res} # DENV
 fdf[[5]] <- function(Temp, t_res){(Temp-14/130)/t_res} # diro
 fdf[[6]] <- function(Temp, t_res){(0.000126*Temp+(Temp-14.244)*sqrt(34.4-Temp))/t_res} # malaria
 
+#eips <- function(Temp, pathogen = "WNV", t_res = 24){
+#  res <- matrix(nrow = length(Temp) * length(pathogen), ncol = 2)
+#  colnames(res) <- c("eip", "pathogen")
+#  vec <-  which((c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria") %in% c(pathogen)) == T)
+#  res[,1] <- unlist(lapply(fdf[c(vec)], function(x) x(Temp, t_res)))
+#  res[,2] <- rep(c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria")[vec], each = length(Temp))
+#  return(data.frame(res))
+#}
 
-eips <- function(Temp, pathogen = "WNV", t_res = 24){
-  vec <-  which((c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria") %in% c(pathogen)) == T)
-  res <- lapply(fdf[c(vec)], function(x) x(Temp, t_res))
-  res2 <- data.frame(do.call(cbind, res))
-  dimnames(res2)[[2]] <- c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria")[vec]
-  return(res2)
+path <- c("BTV", "WNV", "SBV", "DENV", "dirofilaria", "malaria")
+
+eips <- function(T_data = data, T_var = TT_TU, pathogen = "WNV", t_res = 24){
+
+  vec <-  which((path %in% c(pathogen)) == T)
+
+  ggg <- lapply(vec, function(x){
+    T_data %>%
+      group_by(ID) %>%
+      mutate(eips = fdf[[x]](TT_TU, 24), pathogen = path[x])
+  })
+
+  do.call(rbind, ggg)
 }
 
